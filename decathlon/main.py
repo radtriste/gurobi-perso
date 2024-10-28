d7c01f724ff8474fd371038bbd791f7f671290dd
radtriste <tradisso@redhat.com>
2024-10-28 17:34:33 +0100

    Updated decathlon model

--- decathlon/main.py
+++ decathlon/main.py
@@ -3,24 +3,24 @@ import pandas as pd
 import gurobipy as gp
 from gurobipy import GRB
 
-decathlon_card_points = [6,11,16,22,27,53,79,84,94,105,131,157]
-leroy_card_points = [6,11,16,21,27,52,78,83,93,104,130,155]
+# DATA
 
-# Define a gurobipy model for the decision problem
+max_points = 336
+decathlon_card_points = [6,11,16,22,27,53,79,84,94,105,131,157]
+
+# MODEL
 m = gp.Model('cards')
 
+# vars
 decathlon_nb_cards = m.addVars(decathlon_card_points, vtype=GRB.INTEGER, name = "decathlon_card_points")
-decathlon_total_per_card = m.addVars(decathlon_card_points, vtype=GRB.INTEGER, name = "decathlon_card_points")
+# decathlon_total_per_card = m.addVars(decathlon_card_points, vtype=GRB.INTEGER, name = "decathlon_card_points")
 
-leroy_nb_cards = m.addVars(leroy_card_points, vtype=GRB.INTEGER, name = "leroy_nb_cards")
-leroy_total_per_card = m.addVars(leroy_card_points, vtype=GRB.INTEGER, name = "leroy_total_per_card")
+# constraints
+# decathlon_max_points_per_card = m.addConstrs((v * decathlon_nb_cards[v] == decathlon_total_per_card[v] for v in decathlon_card_points), name = "max_points_per_card")
+# decathlon_max_points = m.addConstr((decathlon_total_per_card.sum() == max_points), name = "max_points")
+card_total = gp.quicksum(p*decathlon_nb_cards[p] for p in decathlon_card_points)
+m.addConstr(card_total == max_points, name = "card_total")
 
-m.update()
-print(decathlon_nb_cards)
-print(decathlon_total_per_card)
-
-decathlon_max_points_per_card = m.addConstrs((v * decathlon_nb_cards[v] == decathlon_total_per_card[v] for v in decathlon_card_points), name = "max_points_per_card")
-decathlon_max_points = m.addConstr((decathlon_total_per_card.sum() == 336), name = "max_points")
 m.update()
 
 # m.setObjective(gp.quicksum(total_per_card[v] for v in card_points), GRB.MAXIMIZE)
