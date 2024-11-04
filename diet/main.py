71cb21c8abf96a7389052806e69abc3d1438f623
radtriste <tradisso@redhat.com>
2024-11-04 12:43:41 +0100

    updated diet

--- diet/main.py
+++ diet/main.py
@@ -1,67 +1,94 @@
 
 import gurobipy as gp
+import pandas as pd
+from gurobipy import GRB
 
 # DATA
 # load or import data
 
-categories = {
-    'calories',
-    'proteins'
-}
+categories, minNutrition, maxNutrition = gp.multidict({
+    'calories': [1800, 2200],
+    'proteins': [91, GRB.INFINITY],
+    'fat': [0, 65],
+    'sodium': [0, 1779],
+})
 
-minNutrition = {
-    'calories': 1800,
-    'proteins': 91
-}
-
-maxNutrition = {
-    'calories': 2200,
-    'proteins': gp.GRB.INFINITY
-}
-
-foods = {
-    'salad',
-    'ice_cream'
-}
-
-cost = {
+foods, cost = gp.multidict({
+    'hamburger': 2.49,
+    'chicken': 2.89,
+    'hot_dog': 1.50,
+    'fries': 1.89,
+    'macaroni': 2.09,
+    'pizza': 1.99,
     'salad': 2.49,
+    'milk': 0.89,
     'ice_cream': 1.59
-}
+})
 
-nutritionValues = {
-    ('salad', 'calories'): 320,
-    ('salad', 'proteins'): 21,
-    ('ice_cream', 'calories'): 330,
-    ('ice_cream', 'proteins'): 8,
-}
+nutrition = pd.read_csv('data/nutritionValues.csv', index_col=[0,1]).squeeze('columns')
+# nutrition = nutrition.to_dict()
 
-# MODEL
-# build our model
+def solve(categories, minNutrition, maxNutrition, foods, cost, nutritionValues):
 
-# define a model object
-m = gp.Model('diet')
+    # MODEL
+    # build our model
 
-# introduce the vars
-buy = m.addVars(foods, name="buy")
+    # define a model object
+    m = gp.Model('diet')
 
-# define the objective
-totalCost = gp.quicksum(cost[f] * buy[f] for f in foods)
-m.setObjective(totalCost, gp.GRB.MINIMIZE)
+    # introduce the vars
+    buy = m.addVars(foods, name="buy")
 
-# define the constraints
-for c in categories:
-    categoryTotal = gp.quicksum(buy[f] * nutritionValues[f, c] for f in foods)
-    m.addConstr(minNutrition[c] <= categoryTotal, name="total_"+c+"_min")
-    m.addConstr(maxNutrition[c] >= categoryTotal, name="total_"+c+"_max")
+    # define the objective
+    # totalCost = gp.quicksum(cost[f] * buy[f] for f in foods)
+    # m.setObjective(totalCost, GRB.MINIMIZE)
+    m.setObjective(buy.prod(cost), GRB.MINIMIZE)
 
-# optionally, write out the model to a disc
-m.write('dietTest.lp')
+    # define the constraints
+    # for c in categories:
+    #     categoryTotal = gp.quicksum(buy[f] * nutritionValues[f, c] for f in foods)
+    #     m.addConstr(minNutrition[c] <= categoryTotal, name="total_"+c+"_min")
+    #     m.addConstr(maxNutrition[c] >= categoryTotal, name="total_"+c+"_max")
+    m.addConstrs((gp.quicksum(buy[f] * nutritionValues[f, c] for f in foods) ==
+                  [minNutrition[c], maxNutrition[c]] for c in categories), name="nutrition_req")
 
-# SOLVE & POSTPROCESS
-# optimize
-m.optimize()
+    def print_solution():
+        if m.status == GRB.OPTIMAL:
+            sol = pd.Series(m.getAttr("X", buy), name = "food_bought", index = foods)
+            print(sol[sol > 0])
 
-# review the results
-for f in foods:
-    print("purchase {0} units of {1}".format(buy[f].x, f))
+            nutrition = {}
+            print(f"\nCost: {round(m.ObjVal, 4)}")
+            print("\nBuy:")
+            for f in foods:
+                print(f"\t{f}: {round(buy[f].x, 4)}")
+            print("\nNutrition values:")
+            for c in categories:
+                nutrition[c] = sum(buy[f].x * nutritionValues[f, c]
+                                   for f in foods)
+                print(f"\t{c}: {round(nutrition[c], 4)}")
+        else:
+            print("No solution")
+
+    # optionally, write out the model to a disc
+    m.write('dietTest.lp')
+
+    # SOLVE & POSTPROCESS
+    # optimize
+    m.optimize()
+
+    print_solution()
+
+
+solve(categories, minNutrition, maxNutrition, foods, cost, nutrition)
+
+
+# CHANGE DATA and solve again
+categories, minNutrition, maxNutrition = gp.multidict({
+    'calories': [1500, 1800],
+    'proteins': [100, GRB.INFINITY],
+    'fat': [0, 65],
+    'sodium': [0, 1779],
+})
+
+solve(categories, minNutrition, maxNutrition, foods, cost, nutrition)
